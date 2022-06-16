cca8cc503f275ac1294e63251dfcfafe321771bd
JaY 3 <jeanalainpro2@gmail.com>
2022-06-16 23:09:40 +0200

    policy iteration

--- policy_value_iteration/policy_iteration.py
+++
@@ -1,9 +0,0 @@
-import gym
-import matplotlib.pyplot as plt
-import numpy as np
-
-#---------------------
-# Initialisation
-env=gym.make("FrozenLake-v1")
-transition_probs=env.P
-env.reset(return_info=True)
